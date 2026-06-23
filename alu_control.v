8595581a213cb4dc9987cf852c1bd00a2397a14d
tkoetting03 <141885187+tkoetting03@users.noreply.github.com>
2026-06-23 20:10:53 +0000

    Main control module, added register mux file

--- alu_control.v
+++ alu_control.v
@@ -8,21 +8,21 @@ module alu_control (
 always @(*) begin
     case (alu_operation)
         2'b00: begin // Memory Access
-            alu_control_out = 4'b0000; // Address offset addition
+            alu_control_out = 4'b0000; 
         end 
         2'b01: begin // Branching
-            alu_control_out = 4'b0001; // Branching condition validation
+            alu_control_out = 4'b0001; 
         end
         2'b10: begin // Arithmetic
             case (funct3)
-                3'b000: alu_control_out = (bit30) ? 4'b0001 : 4'b0000; // If bit30 = 1 SUB, if bit30 = 0 ADD
+                3'b000: alu_control_out = (bit30) ? 4'b0001 : 4'b0000; 
                 3'b001: alu_control_out = 4'b0010; // SLL
                 3'b010: alu_control_out = 4'b0011; // SLT
                 3'b011: alu_control_out = 4'b0100; // SLTU
                 3'b100: alu_control_out = 4'b0101; // XOR
                 3'b101: alu_control_out = 4'b0110; // OR
                 3'b110: alu_control_out = 4'b0110; // AND
-                3'b111: alu_control_out = (bit30) ? 4'b1000 : 4'b1001; // If bit30 = 1 SRL, if bit30 = 0 SRA
+                3'b111: alu_control_out = (bit30) ? 4'b1000 : 4'b1001; 
                 default: alu_control_out = 4'b0000;
             endcase
         end
---
+++ main.v
@@ -0,0 +1,73 @@
+module main (
+    input wire[6:0] opcode,
+    output reg branch,
+    output reg memory_read,
+    output reg memory_register,
+    output reg[1:0] alu_operation,
+    output reg memory_write,
+    output reg alu_source,
+    output reg register_write
+);
+
+always @(*) begin
+    case (opcode)
+        7'b0110011: begin // R-type
+            branch = 1'b0;
+            memory_read = 1'b0;
+            memory_register = 1'b0;
+            alu_operation = 2'b10;
+            memory_write = 1'b0;
+            alu_source = 1'b0;
+            register_write = 1'b1;
+        end 
+        7'b0010011: begin // I-type
+            branch = 1'b0;
+            memory_read = 1'b0;
+            memory_register = 1'b0;
+            alu_operation = 2'b10;
+            memory_write = 1'b0;
+            alu_source = 1'b1;
+            register_write = 1'b1;
+        end 
+        7'b0000011: begin // lw
+            branch = 1'b0;
+            memory_read = 1'b1;
+            memory_register = 1'b1;
+            alu_operation = 2'b00;
+            memory_write = 1'b0;
+            alu_source = 1'b1;
+            register_write = 1'b1;
+        end 
+        7'b0110011: begin // sw
+            branch = 1'b0;
+            memory_read = 1'b0;
+            memory_register = 1'b0;
+            alu_operation = 2'b00;
+            memory_write = 1'b1;
+            alu_source = 1'b1;
+            register_write = 1'b0;
+        end 
+        7'b0110011: begin // Branching
+            branch = 1'b1;
+            memory_read = 1'b0;
+            memory_register = 1'b0;
+            alu_operation = 2'b01;
+            memory_write = 1'b0;
+            alu_source = 1'b0;
+            register_write = 1'b0;
+        end 
+        default: begin // 
+            branch = 1'b0;
+            memory_read = 1'b0;
+            memory_register = 1'b0;
+            alu_operation = 2'b00;
+            memory_write = 1'b0;
+            alu_source = 1'b0;
+            register_write = 1'b0;
+        end 
+        
+        default: 
+    endcase
+end
+
+endmodule
